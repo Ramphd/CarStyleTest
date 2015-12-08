1bc595b91cad89943afb9c843af872452ba2b7cd
cari <cari@Yao-Y480>
2015-12-08 12:08:34 +0800

    # try to add some threads things

--- src/test/socket/test/Client.java
+++ src/test/socket/test/Client.java
@@ -2,7 +2,6 @@ package test.socket.test;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -11,65 +10,65 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.concurrent.Callable;
+
 import com.alibaba.fastjson.JSON;
 
-public class Client {
+public class Client implements Callable<String[]> {
 	private static int portNum = 2333;
-	private Socket socket = null;
-	private BufferedReader in = null;
-	private PrintWriter out = null;
-	private String caffe_root = File.separator + "home" + File.separator + "cari" + File.separator + 
-			"caffe" + File.separator;
-	public Client() throws IOException {
+	private Socket socket;
+	private BufferedReader in;
+	private PrintWriter out;
+	private String execode;
+
+	public Client(String execode) throws IOException {
 		// TODO Auto-generated constructor stub
 		InetAddress addr = InetAddress.getByName("localhost");
-		socket = new Socket(addr,portNum);
+		socket = new Socket(addr, portNum);
+		socket.setSoLinger(true, 30);
 		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		out = new PrintWriter(new BufferedWriter(
-				new OutputStreamWriter(socket.getOutputStream())), true);
+		out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+		this.execode = execode;
+
 	}
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		//Client c = new Client();
-		
-	
-	}
+
 	@SuppressWarnings("unchecked")
-	public void oneDirMultiCars() throws IOException{
-		try{
-			System.out.println("This func is oneDirMultiCars and this socket is " + socket);
-			String imageDir = caffe_root + "CarStyle_test" + File.separator;
-			String execode = "c|" + imageDir;
-			//send execode to Server
+	private String[] carStylePRun() throws IOException {
+		try {
+			System.out.println(socket);
 			out.println(execode);
-			System.out.println(execode + "has been sended");
-			//Receive json_image_name_list
+			System.out.println("'" + execode + "'" + " has been sended");
+			// Receive json_image_name_list
 			String j_image_name_list = in.readLine();
-			System.out.println(j_image_name_list);
-			//send json_image_name_list feedback
+			System.out.println("json_: " + j_image_name_list);
+			// send json_image_name_list feedback
 			out.println("imageNameList has been received");
-			//receive json_predict_result 
+			// receive json_predict_result
 			String j_predict_result = in.readLine();
-			System.out.println(j_predict_result);
-			//send json_predict_result feedback
+			System.out.println("json_: " + j_predict_result);
+			// send json_predict_result feedback
 			out.println("predictResult has been received");
-			ArrayList<String> al = new ArrayList<String>();
-			al = JSON.parseObject(j_image_name_list, ArrayList.class);
-			System.out.println("the first image is :" + al.get(1));
-			HashMap<String,ArrayList<String>> hm = new HashMap<String,ArrayList<String>>();
-			hm = JSON.parseObject(j_predict_result, HashMap.class);
-			System.out.println("total predict result is :" + hm);
-		}finally{
+//			ArrayList<String> al = new ArrayList<String>();
+//			al = JSON.parseObject(j_image_name_list, ArrayList.class);
+//			System.out.println("the first image is :" + al);
+//			HashMap<String, ArrayList<String>> hm = new HashMap<String, ArrayList<String>>();
+//			hm = JSON.parseObject(j_predict_result, HashMap.class);
+//			System.out.println("total predict result is :" + hm);
+			String[] rstr = new String[2];
+			rstr[0] = j_image_name_list;
+			rstr[1] = j_predict_result;
+			return rstr;
+		} finally {
 			System.out.println("close the Client socket and the io.");
 			in.close();
 			out.close();
-            socket.close();
+			socket.close();
 		}
 	}
-	public void singleCar(String imageAddr){
-		
-	}
-	public void oneImageMultiCars(String imageAddr){
-		
+
+	@Override
+	public String[] call() throws IOException {
+		// TODO Auto-generated method stub
+		return carStylePRun();
 	}
 }
--- src/test/socket/test/GetInfoTest.java
+++ src/test/socket/test/GetInfoTest.java
@@ -1,19 +1,30 @@
 package test.socket.test;
+import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
 
-import test.socket.test.Client;
+
 public class GetInfoTest {
-
-	public static void main(String[] args) {
+	private static String dir = "CarStyle_test" + File.separator;
+	private static String imagePath = "CarStyle_test" + File.separator + "0019_66.jpg";
+	
+	public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
 		// TODO Auto-generated method stub
-		try {
-			Client c = new Client();
-			c.oneDirMultiCars();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+		HashMap<String,Object> hm1 = new HashMap<String,Object>();
+		HashMap<String,Object> hm2 = new HashMap<String,Object>();
+		ClientRunImpl cri = new ClientRunImpl();
+		hm1 = cri.oneDirMultiCars(dir);
+		hm2 = cri.singleCar(imagePath);
+		System.out.println(hm2 + "\n" + hm1);
+//		System.out.println(cri.oneDirMultiCars(dir));
+//		System.out.println(cri.singleCar(imagePath));
+//		System.out.println(cri.oneDirMultiCars(dir));
+//		System.out.println(cri.singleCar(imagePath));
+//		System.out.println(cri.oneDirMultiCars(dir));
+//		System.out.println(cri.singleCar(imagePath));
+		System.out.println("main");
+		//cri.terminal();
 	}
 
 }
